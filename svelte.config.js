58e8853c570183782a9fb9974ceea70c9d3e30f8
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-12-02 03:26:14 +0700

    fix: add path

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
+const dev = 'production' === 'development'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       pages: 'dist',
       assets: 'dist',
+      paths: {
+        // change below to your repo name
+        base: dev ? '' : '/utm-builder',
+      },
     }),
   },
   preprocess: [

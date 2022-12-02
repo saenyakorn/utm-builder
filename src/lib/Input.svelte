67ea8b6b2ff76692b3cc8743a6bfcc71f19ba9fa
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-12-02 20:43:44 +0700

    feat: add more information in form

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -4,26 +4,38 @@
   export let id: string
   export let value: string
   export let label: string = ''
+  export let helperText: string = ''
 
   interface $$Props extends svelte.JSX.SvelteInputProps {
     id: string
     value: string
     label?: string
+    helperText?: string
   }
 </script>
 
-<div class="flex flex-col gap-2 w-full">
-  {#if label}
-    <label for={id}>{label}</label>
+<div>
+  <div class="flex flex-col gap-2 w-full">
+    {#if label}
+      <label
+        for={id}
+        class={clsx(
+          $$restProps.required && "after:content-['*'] after:text-red-500 after:ml-1 align-middle",
+        )}>{label}</label
+      >
+    {/if}
+    <input
+      type="text"
+      {...$$restProps}
+      bind:value
+      class={clsx(
+        'w-full border-2 border-solid border-gray-300 p-2 rounded-md',
+        'hover:border-blue-400',
+        'transition-colors',
+      )}
+    />
+  </div>
+  {#if helperText}
+    <span class="text-xs text-gray-400">{helperText}</span>
   {/if}
-  <input
-    type="text"
-    {...$$restProps}
-    bind:value
-    class={clsx(
-      'w-full border-2 border-solid border-gray-300 p-2 rounded-md',
-      'hover:border-blue-400',
-      'transition-colors',
-    )}
-  />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
       id="websiteURL"
       label="Website URL"
       placeholder="https://google.com"
+      helperText="The full website URL (e.g. https://www.example.com)"
       required
     />
     <Input
@@ -28,12 +29,14 @@
       id="campaignId"
       label="Campaign Id"
       placeholder="campaign Id..."
+      helperText="The campaign id (e.g. abc.123)"
     />
     <Input
       bind:value={campaign.campaignSource}
       id="campaignSource"
       label="Campaign source"
       placeholder="campaign source..."
+      helperText="Identify the advertiser, site, publication (e.g. google, newsletter4)"
       required
     />
     <Input
@@ -41,25 +44,29 @@
       id="campaignMedium"
       label="Campaign medium"
       placeholder="campaign medium..."
+      helperText="The advertising or marketing medium (e.g. cpc, banner, email)"
       required
     />
     <Input
       bind:value={campaign.campaignName}
       id="campaignName"
-      label="CampaignName"
+      label="Campaign name"
       placeholder="campaign name..."
+      helperText="The individual campaign name, slogan, promo code, etc. for a product. (e.g. spring_sale)"
     />
     <Input
       bind:value={campaign.campaignTerm}
       id="campaignTerm"
       label="Campaign term"
       placeholder="campaign term..."
+      helperText="Identify the paid keywords (e.g. running+shoes)"
     />
     <Input
       bind:value={campaign.campaignContent}
       id="campaignContent"
       label="Campaign content"
       placeholder="campaign content..."
+      helperText="Use to differentiate ads or links that point to the same URL (e.g. logolink)"
     />
     <ShortUrl bind:campaign />
   </form>
